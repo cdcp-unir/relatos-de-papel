44b4974dfd4ec41de0c11588e560cada82789ae8
Michael Zambrano <maikl@MacBook-Pro-de-Michael.local>
2026-03-28 09:00:16 -0500

    Pantallas libros y Resumen Libro

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../SearchBar/SearchBar.css"
+
+function SearchBar() {
+    return (
+        <input type="text" placeholder="Buscar..." />
+    );
+}
+
+export default SearchBar;
