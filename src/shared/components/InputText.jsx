c3b378423b3e8c205e7038817e87bc8584d15ebe
Will <desarrollo@ideasgroup.com.ec>
2026-03-28 00:30:34 -0500

    Agregar checkbox y acción para ocultar y mostrar password

--- src/shared/components/InputText.jsx
+++ src/shared/components/InputText.jsx
@@ -1,4 +1,7 @@
+import { useState } from "react";
 import "./InputText.css"
+import { InputCheckbox } from "./InputCheckbox";
+
 export const InputText = ({ label, id, name, value, onChange }) => (
   <div className="input-group">
     <label htmlFor={id}>
@@ -8,11 +11,16 @@ export const InputText = ({ label, id, name, value, onChange }) => (
   </div>
 );
 
-export const InputPassword = ({ label, id, name, value, onChange }) => (
-  <div className="input-group">
+export const InputPassword = ({ label, id, name, value, onChange }) => {
+  const [showPassword, setShowPassword] = useState(true);
+
+  return (<div className="input-group">
     <label htmlFor={id}>
       <span>{label}</span>
     </label>
-    <input type="password" id={id} name={name} value={value} onChange={onChange} />
+    <input type={showPassword ? "password" : "text"} id={id} name={name} value={value} onChange={onChange} />
+    <InputCheckbox label="Mostrar contraseña" id={id + "-checkbox"} name={name + "-checkbox"} value={showPassword} onChange={_ => setShowPassword(!showPassword)} />
   </div>
-);
+  )
+}
+
