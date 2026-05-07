0b56d92306782744c19116169f0e417559011170
Adrián Hernández <jandryadrianhg2003@gmail.com>
2026-05-06 22:53:17 -0500

    fix: register page

--- src/features/auth/services/RegisterService.js
+++ src/features/auth/services/RegisterService.js
@@ -1,36 +1,25 @@
-function validateUserExists(users, email, username) {
+function validateUserExists(users, email) {
   const emailExists = users.some((user) => user.email === email);
-  const usernameExists = users.some((user) => user.username === username);
-
-  if (emailExists && usernameExists) {
-    throw new Error("EMAIL_AND_USERNAME_EXISTS");
-  }
-
   if (emailExists) {
     throw new Error("EMAIL_EXISTS");
-  }
-
-  if (usernameExists) {
-    throw new Error("USERNAME_EXISTS");
   }
 
   return false;
 }
 
-export async function register({ email, username, password }) {
+export async function register({ email, password }) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   const users = (await import("@mocks/users.json")).default;
 
-  validateUserExists(users, email, username);
+  validateUserExists(users, email);
   const newUser = {
     id: users.length + 1,
     email,
-    username,
     password,
     role: "user"    
   };
 
   users.push(newUser);
-  console.log("Usuario registrado (mock):", { email, username, password });
+  console.log("Usuario registrado (mock):", { email, password });
   return newUser;
 }
